2a26a6e535569e3d5ce2373c55fbcd17565d4b94
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-05-10 18:52:55 +0300

    Add SecurityConfig logic

--- src/main/java/osinovii/spring/security/authenticationandauthorization/config/SecurityConfig.java
+++ src/main/java/osinovii/spring/security/authenticationandauthorization/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package osinovii.spring.security.authenticationandauthorization.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).build();
+        UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user")).build();
+        UserDetails anton = User.builder().username("alex").password(passwordEncoder.encode("password1")).build();
+        return new InMemoryUserDetailsManager(admin, user, anton);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/apps/welcome").permitAll()
+                        .requestMatchers("api/v1/apps/**").authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
